9d2f9db9b421197be36960b8c9d9ff13df088aee
Vitaliy Kalinkin <v@086.kz>
2019-11-13 22:51:56 +0600

    Add constants

--- constants.py
+++ constants.py
@@ -5,3 +5,10 @@ class Color:
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
     ORANGE = (255, 180, 0)
+
+
+class FieldProperties:
+    CELL_WIDTH = 20  # В пикселях
+    CELL_HEIGHT = 20  # В пикселях
+    WIDTH = 20  # В клетках
+    HEIGHT = 20  # В клетках
