8a50460c91ab2056a17e98ea5c2cc6af4ef8608b
Vitaliy Kalinkin <v@086.kz>
2019-11-14 22:13:29 +0600

    Imrove field.py

--- objects/field.py
+++ objects/field.py
@@ -4,15 +4,18 @@ from objects.base import DrawObject
 
 
 class Field(DrawObject):
-    def __init__(self, game, width=FieldProperties.WIDTH, height=FieldProperties.HEIGHT):
+    def __init__(self, game, x=0, y=0, width=FieldProperties.WIDTH, height=FieldProperties.HEIGHT):
         super().__init__(game)
         self.field = []
         self.width = width  # Ширина поля в клетках
         self.height = height  # Высота поля в клетках
+        self.x = x
+        self.y = y
         for y in range(height):
             self.field += [[]]
             for x in range(width):
-                self.field[-1].append(Cell(game, x, y, 'images/ground.png'))
+                self.field[-1].append(Cell(game, x * FieldProperties.CELL_LENGTH + self.x,
+                                           y * FieldProperties.CELL_LENGTH + self.y, 'images/ground.png'))
 
     def process_draw(self):
         for i in self.field:
@@ -27,8 +30,8 @@ class Cell(DrawObject):
         self.y = y
         self.image = pygame.image.load(image)
         self.rect = self.image.get_rect()
-        self.rect.x = x * FieldProperties.CELL_LENGTH
-        self.rect.y = y * FieldProperties.CELL_LENGTH
+        self.rect.x = x
+        self.rect.y = y
         self.rect.width = self.rect.height = FieldProperties.CELL_LENGTH
 
     def process_draw(self):
