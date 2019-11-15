1a5428402c9b2c46fe2defcba2bfea857ce3bcfa
timikos <timikos@mail.ru>
2019-11-15 13:34:57 +0300

    Version 0.51 fixed main.py

--- scenes/main.py
+++ scenes/main.py
@@ -1,8 +1,5 @@
-from constants import Color
-
 from objects.bomberman import Bomberman
 from objects.field import Field
-from objects.text import Text
 from scenes.base import Scene
 from objects.ghosts import Ghost
 
@@ -10,10 +7,9 @@ class MainScene(Scene):
 
     def create_objects(self):
         self.ghosts = [Ghost(self.game) for _ in range(5)]
-        self.text_count = Text(self.game, text='', color=Color.RED, x=400, y=550)
         self.bomberman = Bomberman(self.game)
         self.field = Field(self.game)
-        self.objects = [self.field] + [self.text_count] + [self.bomberman] + self.ghosts
+        self.objects = [self.field] + [self.bomberman] + self.ghosts
 
     def additional_logic(self):
         self.process_ghost_collisions()
