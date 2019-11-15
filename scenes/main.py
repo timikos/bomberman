cabcc0d8345705bd92c4b96a51a91bca439778b2
Aitaza Akylgali <akylsultan02@gmail.com>
2019-11-15 22:43:39 +0300

    ball residues deleted

--- scenes/main.py
+++ scenes/main.py
@@ -1,5 +1,4 @@
 from constants import Color
-from objects.balls import LinearMovingBall
 from objects.text import Text
 from scenes.base import Scene
 
@@ -9,8 +8,7 @@ class MainScene(Scene):
 
     def create_objects(self):
         self.text_count = Text(self.game, text='', color=Color.RED, x=400, y=550)
-        self.balls = [LinearMovingBall(self.game) for _ in range(5)]
-        self.objects = self.balls + [self.text_count]
+        self.objects = [self.text_count]
 
     def additional_logic(self):
         self.process_ball_collisions()
@@ -22,10 +20,3 @@ class MainScene(Scene):
         )
         if self.game.wall_collision_count >= self.MAX_COLLISIONS:
             self.set_next_scene(self.game.GAMEOVER_SCENE_INDEX)
-
-    def process_ball_collisions(self):
-        for i in range(len(self.balls) - 1):
-            for j in range(i + 1, len(self.balls)):
-                if self.balls[i].collides_with(self.balls[j]):
-                    print('Мячи {} и {} столкнулись'.format(i, j))
-                    self.balls[i].collision(self.balls[j])
