6cfd2bbb575788e6559bcaf052e2869eaa82674b
Timur <31807569+timikos@users.noreply.github.com>
2019-11-17 16:52:18 +0300

    Version 0.6 Merge bomberman with branch score

--- scenes/main.py
+++ scenes/main.py
@@ -2,14 +2,16 @@ from objects.bomberman import Bomberman
 from objects.field import Field
 from scenes.base import Scene
 from objects.ghosts import Ghost
+from objects.score import Score
 
 
 class MainScene(Scene):
     def create_objects(self):
         self.ghosts = [Ghost(self.game) for _ in range(5)]
         self.bomberman = Bomberman(self.game)
+        self.score = Score(self.game)
         self.field = Field(self.game)
-        self.objects = [self.field] + [self.bomberman] + self.ghosts
+        self.objects = [self.field] + [self.bomberman] + self.ghosts + [self.score]
 
     def additional_logic(self):
         self.process_ghost_collisions()
