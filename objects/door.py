63b79de64697af7d0794c5e37c754cd2f9697189
Anton Vinogradov <nello1989@mail.ru>
2019-11-18 20:39:42 +0300

    Change Door class and add process_door_collisions to MainScene

--- objects/door.py
+++ objects/door.py
@@ -6,12 +6,14 @@ from objects.base import DrawObject
 class Door(DrawObject):
     filename = 'images/door/door.png'
 
-    def __init__(self, game, x=100, y=100):
+    def __init__(self, game, x=40, y=40):
         super().__init__(game)
         self.image = pygame.image.load(Door.filename)
         self.x = x
         self.y = y
         self.rect = self.image.get_rect()  # rect необходим для коллизии игрока с дверью
+        self.rect.x = 120
+        self.rect.y = 400
 
     def process_event(self, event):
         pass
@@ -19,13 +21,13 @@ class Door(DrawObject):
     def process_logic(self):
         pass
 
-    def show_door(self):
-        """Метод появления двери. Дверь появляется при условии, что все призраки убиты."""
-        pass
-
     def collides_with(self, bomberman):
         """Персонаж должен зайти в дверь для завершения уровня"""
-        pass
+        return self.rect.colliderect(bomberman)
 
     def process_draw(self):
+        """Метод появления двери. Дверь появляется при условии, что все призраки убиты."""
+        # Дверь не должна появляться на месте неразрушаемых блоков
+        # Добавить импорт ghosts
+        # if len(self.ghosts) == 0
         self.game.screen.blit(self.image, self.rect)
--- scenes/main.py
+++ scenes/main.py
@@ -3,6 +3,7 @@ from objects.field import Field
 from scenes.base import Scene
 from objects.ghosts import Ghost
 from objects.score import Score
+from objects.door import Door
 
 
 class MainScene(Scene):
@@ -11,13 +12,18 @@ class MainScene(Scene):
         self.bomberman = Bomberman(self.game)
         self.score = Score(self.game)
         self.field = Field(self.game)
-        self.objects = [self.field] + [self.bomberman] + self.ghosts + [self.score]
+        self.door = Door(self.game)
+        self.objects = [self.field] + [self.bomberman] + self.ghosts + [self.score] + [self.door]
 
     def additional_logic(self):
         self.process_ghost_collisions()
-
 
     def process_ghost_collisions(self):
         for ghost in self.ghosts:  # Коллизия бомбермэна с призраками
             if ghost.collides_with(self.bomberman):
                 print('col')
+
+    def process_door_collisions(self):
+        # Бомбермэн входит в дверь
+        if self.door.collides_with(self.bomberman):
+            print('Level up!')
