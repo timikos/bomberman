6e06b376cad49b3b6241b2d4e276611691a869e2
Aleksandr Pozdnyakov <aa.pozdnyak@yandex.ru>
2019-11-19 00:49:31 +0300

    added bomb

--- scenes/main.py
+++ scenes/main.py
@@ -1,31 +1,27 @@
-from constants import Color
-from objects.balls import LinearMovingBall
-from objects.text import Text
+# from objects.bomberman import Bomberman
+from objects.bombs import Bomb
+# from objects.field import Field
 from scenes.base import Scene
+# from objects.ghosts import Ghost
+# from objects.score import Score
 
 
 class MainScene(Scene):
-    MAX_COLLISIONS = 15
-
     def create_objects(self):
-        self.text_count = Text(self.game, text='', color=Color.RED, x=400, y=550)
-        self.balls = [LinearMovingBall(self.game) for _ in range(5)]
-        self.objects = self.balls + [self.text_count]
+        # self.ghosts = [Ghost(self.game) for _ in range(5)]
+        # self.bomberman = Bomberman(self.game)
+        # self.score = Score(self.game)
+        # self.field = Field(self.game)
+        self.bomb = Bomb(self.game)
+        # self.objects = [self.field] + [self.bomberman] + self.ghosts + [self.score] + [self.bomb]
+        self.objects = [self.bomb]
 
     def additional_logic(self):
-        self.process_ball_collisions()
-        self.text_count.update_text(
-            'Коллизии со стенами: {}/{}'.format(
-                self.game.wall_collision_count,
-                self.MAX_COLLISIONS
-            )
-        )
-        if self.game.wall_collision_count >= self.MAX_COLLISIONS:
-            self.set_next_scene(self.game.GAMEOVER_SCENE_INDEX)
+        self.process_ghost_collisions()
 
-    def process_ball_collisions(self):
-        for i in range(len(self.balls) - 1):
-            for j in range(i + 1, len(self.balls)):
-                if self.balls[i].collides_with(self.balls[j]):
-                    print('Мячи {} и {} столкнулись'.format(i, j))
-                    self.balls[i].collision(self.balls[j])
+
+    def process_ghost_collisions(self):
+        pass
+        # for ghost in self.ghosts:  # Коллизия бомбермэна с призраками
+        #     if ghost.collides_with(self.bomberman):
+        #         print('col')
