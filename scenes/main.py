0119e65ae46891b6e8061ccf5c146d9c81f437ed
timikos <timikos@mail.ru>
2019-11-19 16:37:38 +0300

    Version 0.9.2 added health, but need fix

--- scenes/main.py
+++ scenes/main.py
@@ -5,10 +5,10 @@ from objects.field import Field
 from scenes.base import Scene
 from objects.ghosts import Ghost
 from objects.blocks import TileMap
-from objects.score import Score
+from objects.score import Score, ScorePos
 from objects.door import Door
 from objects.bombs import Bomb
-
+from constants import Color
 
 class MainScene(Scene):
 
@@ -16,11 +16,12 @@ class MainScene(Scene):
         self.bomberman = Bomberman(self.game)
         self.ghosts = [Ghost(self.game) for _ in range(5)]
         self.score = Score(self.game)
+        self.health = Score(self.game, Color.RED, 5, 60, ScorePos.LEFT_BOTTOM, "Health: ", text_after="", border_shift=(10, 10))
         self.field = Field(self.game)
         self.tilemap = TileMap(self.game)
         self.door = Door(self.game)
         self.bomb = Bomb(self.game)
-        self.objects = [self.field] + [self.tilemap] + [self.bomberman] + self.ghosts + [self.score] + [self.door] + [self.bomb]
+        self.objects = [self.field] + [self.tilemap] + [self.bomberman] + self.ghosts + [self.score] + [self.health] + [self.door] + [self.bomb]
 
 
     def additional_logic(self):
@@ -28,12 +29,12 @@ class MainScene(Scene):
         self.process_door_collisions()
         self.process_ghost_collisions_bomb()
         self.process_show_door()
+        self.process_game_lose()
 
     def process_ghost_collisions(self):
         for ghost in self.ghosts:  # Коллизия бомбермэна с призраками
             if ghost.collides_with(self.bomberman):
-                print('col')
-                self.set_next_scene(self.game.GAMEOVER_SCENE_INDEX)  # Добавлено на время
+                self.health.sub(1)
 
     def process_ghost_collisions_bomb(self):
         for ghost in self.ghosts:  # Коллизия бомбы с призраками
@@ -54,10 +55,16 @@ class MainScene(Scene):
         if self.score.count == 5:
             self.door.show_door()
 
+    def process_game_lose(self):
+        if self.health.count == 0:
+            self.score.write_to_file('score.txt', 'Group 04')
+            self.set_next_scene(self.game.GAMEOVER_SCENE_INDEX)  # Добавлено на время
+
     def process_door_collisions(self):
         # Бомбермэн входит в дверь
         if self.door.collides_with(self.bomberman):
             self.set_next_scene(self.game.GAMEOVER_SCENE_INDEX)
+            self.score.write_to_file('score.txt', 'Группа 04')
 
 
 
