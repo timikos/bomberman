076f9953d61f2ef276b9d97f7847b987fb4c2691
Vitaliy Kalinkin <v@086.kz>
2019-11-23 23:47:46 +0600

    Fixed constants.py according to PEP 8

--- constants.py
+++ constants.py
@@ -7,30 +7,35 @@ class Color:
     ORANGE = (255, 180, 0)
     YELLOW = (255, 179, 0)
 
+
 class ScreenProperties:
-    WIDTH = 800 # Ширина экрана
-    HEIGHT = 670 # Высота экрана
+    WIDTH = 800  # Ширина экрана
+    HEIGHT = 670  # Высота экрана
     SCREEN_BORDER_WIDTH = 160  # Граница экрана по ширине
-    SCREEN_BORDER_HEIGHT = 150 # Граница экрана по высоте
+    SCREEN_BORDER_HEIGHT = 150  # Граница экрана по высоте
+
 
 class FieldProperties:
     CELL_LENGTH = 40  # Длина стороны одной клетки в пикселях
     WIDTH = 31  # В клетках
     HEIGHT = 15  # В клетках
 
+
 class BombermanProperties:
-    RESPAWN_X = 400 # Появление бомбермена на оси х
-    RESPAWN_Y = 300 # Появление бомбермена на оси y
-    DIRECTION_X = 0 # Начальное направление
-    DIRECTION_Y = 0 # Начальное направление
-    WIDTH = 35 # Ширина прямоугольника бомбермена
-    HEIGHT = 40 # Высота прямоугольника бомбермена
+    RESPAWN_X = 400  # Появление бомбермена на оси х
+    RESPAWN_Y = 300  # Появление бомбермена на оси y
+    DIRECTION_X = 0  # Начальное направление
+    DIRECTION_Y = 0  # Начальное направление
+    WIDTH = 35  # Ширина прямоугольника бомбермена
+    HEIGHT = 40  # Высота прямоугольника бомбермена
+
 
 class EnemyProperties:
     WIDTH = 30  # Ширина прямоугольника врагов
     HEIGHT = 35  # Высота прямоугольника врагов
     DIRECTION_X = 0  # Начальное направление
     DIRECTION_Y = 0  # Начальное направление
+
 
 class BombProperties:
     WIDTH = 30  # Ширина прямоугольника бомб
